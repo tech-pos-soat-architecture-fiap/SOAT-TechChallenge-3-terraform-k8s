95fb3d71c03cdd9aae9c728f94073f409a85c687
Edir Lucas Icety <icetylucas@gmail.com>
2025-09-29 17:08:11 -0300

    tetando dar permissao de cluster para o admin

--- TechFood/access-entry.tf
+++ TechFood/access-entry.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_access_entry" "access_entry" {
   cluster_name      = aws_eks_cluster.cluster.name
   principal_arn     = data.aws_iam_user.principal_user.arn
-  kubernetes_groups = ["group-11-soat", "group-22-soat"]
+  kubernetes_groups = ["system:masters"]
   type              = "STANDARD"
 }
 
