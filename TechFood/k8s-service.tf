3137632774a6a42d232cdffb780a4edc54ece62d
Rodrigo Soares <54247115+rodrigosoare5@users.noreply.github.com>
2025-10-06 00:51:17 -0300

    Update k8s-service.tf

--- TechFood/k8s-service.tf
+++ TechFood/k8s-service.tf
@@ -6,13 +6,17 @@ kind: Service
 metadata:
   name: tech-food-service
   namespace: techfood
+  annotations:
+    service.beta.kubernetes.io/aws-load-balancer-type: "nlb"
+    service.beta.kubernetes.io/aws-load-balancer-scheme: "internal"
+    service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: "true"
 spec:
   selector:
     app: techfood
   ports:
     - port: 80
       targetPort: 8000
+      protocol: TCP
   type: LoadBalancer
-  loadBalancerClass: service.k8s.aws/nlb
 YAML
 }
